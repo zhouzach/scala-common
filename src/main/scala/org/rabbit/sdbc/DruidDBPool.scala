11a3fc1953bef867ca5e99f56e406a342e7d36b4
Zhou Zach <wander669@gmail.com>
2019-03-20 17:12:24 +0800

    refactor druid db pool

--- src/main/scala/org/rabbit/sdbc/DruidDBPool.scala
+++ src/main/scala/org/rabbit/sdbc/DruidDBPool.scala
@@ -3,7 +3,10 @@ package org.rabbit.sdbc
 import java.sql.{Connection, ResultSet, SQLException}
 
 import com.alibaba.druid.pool.DruidDataSource
+import org.rabbit.config.FileConfig
 import org.rabbit.sdbc.C3P0Tools.{close, logger}
+
+import scala.collection.mutable.Map
 
 object DruidDBPool {
 
@@ -61,5 +64,40 @@ object DruidDBPool {
     }
   }
 
+  def main(args: Array[String]): Unit = {
+    val mysqlConfig = FileConfig.mysqlConfig
+    val url = mysqlConfig.getString("url")
+    val driverClassName = mysqlConfig.getString("driverClassName")
+    val username = mysqlConfig.getString("username")
+    val password = mysqlConfig.getString("password")
+
+    val sql = "select * from XXL_JOB_QRTZ_CRON_TRIGGERS"
+
+
+    val dataSource = DruidDBPool.getDataSource(url, driverClassName, username, password)
+
+
+    val fun2CalN = (rs: Option[ResultSet]) => {
+
+      val map = Map.empty[String, Any]
+      rs match {
+        case Some(result) =>
+          while (result.next()) {
+            val name = result.getString("SCHED_NAME")
+            //              val age = result.getInt("TRIGGER_GROUP")
+            val age = result.getString("TRIGGER_GROUP")
+            map += (name -> age)
+          }
+          map
+        case None => map
+      }
+    }
+
+    DruidDBPool.execQuery(Some(dataSource.getConnection()), sql)(fun2CalN).foreach {
+      case (k, v) =>
+        println(k)
+        println(v)
+    }
+  }
 
 }
