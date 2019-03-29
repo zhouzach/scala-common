d54d1265e631e758fffdbf66bcf6d2c587f784af
Zach Zhou <ispmd@foxmail.com>
2019-03-29 14:45:28 +0800

    add DruidUtil

---
+++ src/main/scala/org/rabbit/utils/DruidUtil.scala
@@ -0,0 +1,204 @@
+package org.rabbit.utils
+
+import java.sql.{Connection, PreparedStatement, ResultSet, SQLException}
+
+import com.alibaba.druid.pool.DruidDataSource
+import org.slf4j.LoggerFactory
+
+object DruidUtil {
+
+  private val logger = LoggerFactory.getLogger(getClass)
+  private val dataSource = new DruidDataSource()
+
+  def initDataSource(url: String, driverClassName: String, username: String, password: String) = {
+
+    dataSource.setUrl(url)
+    dataSource.setDriverClassName(driverClassName)
+    dataSource.setUsername(username)
+    dataSource.setPassword(password)
+
+    dataSource.setInitialSize(10)
+    dataSource.setMinIdle(10)
+    dataSource.setMaxActive(100)
+
+    dataSource.setMaxWait(60000)
+    dataSource.setTimeBetweenEvictionRunsMillis(60000)
+    dataSource.setMinEvictableIdleTimeMillis(300000)
+
+    /**
+      * 让连接池知道数据库已经断开了，并且自动测试连接查询:
+      * 用来检测连接是否有效的sql，要求是一个查询语句，常用select 'x'。
+      * 如果validationQuery为null，testOnBorrow、testOnReturn、testWhileIdle都不会起作用
+      */
+    dataSource.setValidationQuery("SELECT 'x'")
+    dataSource.setTestWhileIdle(true)
+    dataSource.setTestOnBorrow(false)
+    dataSource.setTestOnReturn(false)
+
+    dataSource.setPoolPreparedStatements(false)
+    dataSource.setMaxPoolPreparedStatementPerConnectionSize(10)
+
+    try {
+      dataSource.setFilters("stat");
+    } catch {
+      case e: SQLException =>
+        logger.error(e.getMessage)
+    }
+
+    dataSource
+  }
+
+
+
+
+  def getConnection(): Option[Connection] = {
+    try {
+      Some(dataSource.getConnection)
+    } catch {
+      case sqlExp: SQLException =>
+        sqlExp.printStackTrace()
+        logger.info(sqlExp.toString)
+        None
+    }
+  }
+
+  def close(connection: Connection) = {
+    if(connection != null) {
+      try {
+        connection.close()
+      }catch {
+        case sqlExp: SQLException =>
+          sqlExp.printStackTrace()
+          logger.info(sqlExp.toString)
+        case exp: Exception =>
+          exp.printStackTrace()
+          logger.info(exp.toString)
+      }
+    }
+  }
+
+  def execQuery[T](connection: Option[Connection], query: String)(function: Option[ResultSet] => T) = {
+    try {
+      if(connection.nonEmpty) {
+        val statement = connection.get.createStatement()
+        val rs = statement.executeQuery(query)
+        function(Some(rs))
+      } else {
+        function(None)
+      }
+    }catch {
+      case sqlExp: SQLException =>
+        sqlExp.printStackTrace()
+        logger.info(sqlExp.toString)
+        function(None)
+      case exp: Exception =>
+        exp.printStackTrace()
+        logger.info(exp.toString)
+        function(None)
+    }finally {
+      connection match {
+        case Some(conn) => close(connection.get)
+        case None =>
+      }
+    }
+  }
+
+  def execUpdate[T](connection: Option[Connection], updateStr: String) = {
+    try {
+      if(connection.nonEmpty) {
+        val statement = connection.get.createStatement()
+        statement.executeUpdate(updateStr)
+      }
+    }catch {
+      case sqlExp: SQLException =>
+        sqlExp.printStackTrace()
+        logger.info(sqlExp.toString)
+      case exp: Exception =>
+        exp.printStackTrace()
+        logger.info(exp.toString)
+    }finally {
+      connection match {
+        case Some(conn) => close(connection.get)
+        case None =>
+      }
+    }
+  }
+
+
+  def executeBatch[T](connection: Option[Connection], insertSql: String)(function: Option[PreparedStatement] => T) = {
+
+    try {
+      if(connection.nonEmpty) {
+        val preparedStmt = connection.get.prepareStatement(insertSql)
+
+        function(Some(preparedStmt))
+      } else {
+        function(None)
+      }
+    }catch {
+      case sqlExp: SQLException =>
+        sqlExp.printStackTrace()
+        logger.info(sqlExp.toString)
+        function(None)
+
+    }finally {
+      connection match {
+        case Some(conn) => close(connection.get)
+        case None =>
+      }
+    }
+
+  }
+
+  def main(args: Array[String]): Unit = {
+
+    initDataSource("", "", "","")
+
+    val insertSql = "INSERT INTO APP.PARTSUPP VALUES(?, ?, ?, ?)"
+
+    val tenRecords = (preparedStmtOpt: Option[PreparedStatement]) => {
+
+      preparedStmtOpt match {
+        case Some(preparedStmt) =>
+
+          for (x <- 1 to 10) {
+            preparedStmt.setInt(1, x*100)
+            preparedStmt.setInt(2, x)
+            preparedStmt.setInt(3, x*1000)
+            preparedStmt.setBigDecimal(4, java.math.BigDecimal.valueOf(100.2))
+            preparedStmt.addBatch()
+          }
+          preparedStmt.executeBatch()
+
+          preparedStmt.close()
+
+        case None =>
+      }
+    }
+    executeBatch(getConnection(),insertSql)(tenRecords)
+
+
+    val func = (rs: Option[ResultSet]) => {
+
+      var res: Seq[String] = Seq()
+
+      rs match {
+        case Some(result) =>
+          while(result.next()) {
+              val appId = result.getString("appid")
+              result.getString("expid")
+              result.getInt("n")
+
+            res = res :+ appId
+          }
+
+          res
+        case None => res
+      }
+    }
+    execQuery(getConnection(), "select * from t1")(func)
+
+  }
+
+
+}
