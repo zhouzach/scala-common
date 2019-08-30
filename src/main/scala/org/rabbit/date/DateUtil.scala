b3f8a1c927e84bf680441cb47747b9e38b4974e2
Zhou Zach <wander669@gmail.com>
2019-08-30 14:44:11 +0800

    complete DateUtil

--- src/main/scala/org/rabbit/date/DateUtil.scala
+++ src/main/scala/org/rabbit/date/DateUtil.scala
@@ -105,35 +105,142 @@ object DateUtil {
     dates
   }
 
-  def getYear(date: String): Int ={
+  def formateDate(date: String) = {
+    val dt = new DateTime(date).toString("yyyy-MM-dd")
+    new DateTime(date).toString("yyyy-MM-dd HH:mm:ss")
+    println(dt)
+  }
+
+  def formateDate2yyyyMM(date: String) = {
+    val d = new DateTime(date).toString("yyyy-MM").replaceAll("-", "")
+    println(d)
+  }
+
+  def getBeijingTime(date: String) = {
+    val bjt = new DateTime(date).withZone(DateTimeZone.UTC)
+      .withZone(DateTimeZone.forOffsetHours(8)).toString("yyyy-MM-dd")
+    println(bjt)
+  }
+
+  def getYear(date: String): Int = {
     LocalDate.parse(date).getYear
-
   }
 
-  def getMonth(date: String): Int ={
+  def getMonth(date: String): Int = {
     LocalDate.parse(date).getMonthOfYear
-
   }
-  def getDay(date: String): Int ={
-    LocalDate.parse(date).getDayOfMonth
 
+  def getDay(date: String): Int = {
+    LocalDate.parse(date).getDayOfMonth
+  }
+
+  def getStartTime(date: String) = {
+    val startTime = new DateTime(date).withTimeAtStartOfDay()
+    println(startTime)
+  }
+
+  def getEndTime(date: String) = {
+    val endTime = new DateTime(date).plusDays(1).withTimeAtStartOfDay().plusSeconds(1)
+    println(endTime)
+  }
+
+  def getNewYearDay(date: String) = {
+    val startDay = LocalDate.parse(date).withMonthOfYear(1).withDayOfMonth(1)
+    println(startDay)
+  }
+
+  def get12thMonth(date: String) = {
+
+    val mon = LocalDate.parse(date).withMonthOfYear(1).withDayOfMonth(1).minusDays(1)
+      .getMonthOfYear
+    println(mon)
+  }
+
+  def get1YearAgo(date: String) = {
+    val ayear = new DateTime(date).minusYears(1)
+      .withZone(DateTimeZone.UTC).withZone(DateTimeZone.forOffsetHours(8)).toString("yyyy-MM-dd")
+    println(ayear)
   }
 
   def main(args: Array[String]): Unit = {
-//    getDaysBetweenDate("2018-01-06", "2018-01-07").foreach(println(_))
-//
-//    println()
-//    getMonthsBetweenDate("2018-01", "2018-04").foreach(println(_))
-//
-//    println()
-//    getYearsBetweenDate("2016-10-20", "2018-04-10").foreach(println(_))
+    //    getDaysBetweenDate("2018-01-06", "2018-01-07").foreach(println(_))
+    //
+    //    println()
+    //    getMonthsBetweenDate("2018-01", "2018-04").foreach(println(_))
+    //
+    //    println()
+    //    getYearsBetweenDate("2016-10-20", "2018-04-10").foreach(println(_))
 
-    val date ="2016-07-02"
+    val date = "2019-08-31"
 
-    println(getYear(date))
-    println(getMonth(date))
-    println(getDay(date))
+    get1YearAgo(date)
 
+//    println(
+//      s"""
+//         |in Tag trait:
+//         |
+//         |date: $date
+//         |
+//         |beginDate: $beginDate
+//         |beginYear: $beginYear
+//         |beginMonth1: $beginMonth1
+//         |beginMonth2: $beginMonth2
+//         |
+//         |endYear: $endYear
+//         |endMonth1: $endMonth1
+//         |endMonth2: $endMonth2
+//         |
+//         |orderTimeEndDate: $orderTimeEndDate
+//         |
+//         |cdpOrderBeginDate: $cdpOrderBeginDate
+//         |cdpOrderEndDate: $cdpOrderEndDate
+//         |
+//         |date1m: $date1m
+//         |year1m: $year1m
+//         |month1m: $month1m
+//         |
+//         |""".stripMargin)
+
+
+  }
+
+  def getDates(date: String, n: Int) = {
+
+    val date1m = new DateTime(date)
+      .plusMonths(1)
+      .withZone(DateTimeZone.UTC).withZone(DateTimeZone.forOffsetHours(8))
+    val year1m = date1m.getYear
+    val month1m = date1m.getMonthOfYear
+
+    val dt = new DateTime(date).plusMonths(n)
+    if (dt.getYear > new DateTime(date).getYear) {
+
+      val month12th = dt.withMonthOfYear(1).withDayOfMonth(1).minusDays(1)
+        .withZone(DateTimeZone.UTC).withZone(DateTimeZone.forOffsetHours(8))
+        .getMonthOfYear
+
+      val eYear = dt
+        .withZone(DateTimeZone.UTC).withZone(DateTimeZone.forOffsetHours(8))
+        .getYear
+      val eMonth1 = dt.withMonthOfYear(1).withDayOfMonth(1)
+        .withZone(DateTimeZone.UTC).withZone(DateTimeZone.forOffsetHours(8))
+        .getMonthOfYear
+      val eMonth2 = dt
+        .withZone(DateTimeZone.UTC).withZone(DateTimeZone.forOffsetHours(8))
+        .getMonthOfYear
+
+      s"""
+         |
+         |((order_year = $year1m and order_month between $month1m and $month12th)
+         |or (order_year = $eYear and order_month between $eMonth1 and $eMonth2))
+         |""".stripMargin
+
+    } else {
+
+      s"""
+         |order_year = ${dt.getYear}  and order_month between $month1m and ${dt.getMonthOfYear}
+         |""".stripMargin
+    }
   }
 
 }
